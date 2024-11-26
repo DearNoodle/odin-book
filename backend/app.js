b9244f53a5021c01cab9c5f126179ddecc319c60
DearNoodle <wan619395@gmail.com>
2024-11-26 13:33:57 +0800

    deploy url

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://dearnoodle-odin-book.netlify.app',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
